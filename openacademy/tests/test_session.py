36901554d1f9d6ec6ba1d806c8dad899c15562d4
Farid-mo <faridmouzoune@outlook.fr>
2021-05-04 16:39:20 +0000

    modify tests folder

--- openacademy/tests/test_session.py
+++ openacademy/tests/test_session.py
@@ -1,24 +1,30 @@
 from odoo.tests.common import TransactionCase
-from datetime import date
+import datetime
 import unittest
 
 
 class TestAcademySession(TransactionCase):
     def setUp(self):
         super(TestAcademySession, self).setUp()
+        self.academy_attendee_ids = self.env['res.partner'].create({
+            'name': 'name',
+        })
         self.academy_courses = self.env['academy.courses'].create({
             'name': 'JavaScript'
         })
 
     def test_academy_session(self):
         data = self.env['academy.session'].create({
-            'name': 'Session of JavaScript language',
+            'name': 'Session of JavaScript programing language',
             'instructor_id': self.env.user.id,
             'course_id': self.academy_courses.id,
-
+            'attendee_ids': self.academy_attendee_ids.ids,
+            'start_date': '2021-5-4',
         })
-        self.assertEqual(data.name, 'Session of JavaScript language')
+        self.assertEqual(data.name, 'Session of JavaScript programing language')
         self.assertEqual(data.instructor_id.id, self.env.user.id)
         self.assertEqual(data.course_id.id, self.academy_courses.id)
+        self.assertEqual(data.attendee_ids, self.academy_attendee_ids)
+        self.assertEqual(data.start_date, datetime.date(2021, 5, 4))
 
 
